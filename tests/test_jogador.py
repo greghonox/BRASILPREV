c9c05990dbb237d8f0f1c07871b17ea1974cf33e
gregorio honorato <gregoriohonorato@AZ028.local>
2022-10-19 22:51:34 -0300

    criacao do tipo jogador

---
+++ tests/test_jogador.py
@@ -0,0 +1,12 @@
+from banco_imobiliario.jogador import Jogador
+from unittest import TestCase
+
+
+class TestJogador(TestCase):
+        
+    def test_comprar(self) -> None:
+        """Testa a compras"""
+    
+    def test_pular(self) -> None:
+        """Testa os pulos"""
+        
---
+++ tests/test_tipo_jogador.py
@@ -0,0 +1,32 @@
+from banco_imobiliario.jogador import TipoJogar
+from unittest import TestCase
+
+
+class TestTipoJogador(TestCase):
+        
+    def test_compra_impulsivo(self) -> None:
+        """Como o impulsivo compra"""
+        tipo_jogador = TipoJogar.impulsivo
+        self.assertEqual(tipo_jogador.value['compra'], 100)
+        self.assertTrue(tipo_jogador.comprar_impulsivo())
+
+    def test_compra_cauteloso(self) -> None:
+        """Como o cauteloso compra"""
+        reserva = 80
+        tipo_jogador = TipoJogar.cauteloso
+        self.assertEqual(tipo_jogador.value['compra'], 80)
+        self.assertTrue(tipo_jogador.comprar_cauteloso(reserva))
+        self.assertFalse(tipo_jogador.comprar_cauteloso(reserva - 1))
+
+    def test_compra_aleatorio(self) -> None:
+        """Como o aleatorio compra"""
+        tipo_jogador = TipoJogar.aleatorio
+        self.assertEqual(tipo_jogador.value['compra'], 0)
+        
+    def test_compra_exigente(self) -> None:
+        """Como o exigente compra"""
+        valor_aluguel = 50
+        tipo_jogador = TipoJogar.exigente
+        self.assertEqual(tipo_jogador.value['compra'], 50)
+        self.assertTrue(tipo_jogador.comprar_exigente(valor_aluguel))
+        self.assertFalse(tipo_jogador.comprar_exigente(valor_aluguel - 1))
